a09aaf7c4474442d20a9d8345c70a2a5b7855ec3
Keith Mellea <keithmellea@gmail.com>
2021-09-12 11:42:17 -0700

    commit

---
+++ Joole_Project/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using System.Net.Configuration;
+using Joole_DataAccess;
+using Joole_Project;
+using Joole_Service;
+using TheJooleProject.Models;
+
+namespace Joole_Project.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private JooleDBEntities1 db = new JooleDBEntities1();
+        private IProductService productService = new ProductService();
+        //private ProductService productServiceNew = new ProductService();
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<ProductDetailsVM> model = new List<ProductDetailsVM>();
+
+
+            productService.GetProducts().ToList().ForEach(p =>
+            {
+                ProductDetailsVM vm = feedProductVM(p);
+                model.Add(vm);
+            });
+
+            return View(model);
+        }
+        public ActionResult ProductDetails(long? id)
+        {
+            Product p = productService.GetProduct(id.Value);
+            ProductDetailsVM model = feedProductVM(p);
+            return View("ProductDetails", model);
+        }
+        [HttpGet]
+        public ActionResult ProductsCompare(long? id1, long? id2, long? id3)
+        {
+            long?[] ids = { id1, id2, id3 };
+            List<ProductDetailsVM> model = new List<ProductDetailsVM>();
+            foreach (long? id in ids)
+            {
+                if (id != null && id.Value != -1)
+                {
+                    Product p = productService.GetProduct(id.Value);
+                    ProductDetailsVM vm = feedProductVM(p);
+                    model.Add(vm);
+                }
+            }
+            ViewBag.Model = model;
+            return View();
+        }
+
+        private ProductDetailsVM feedProductVM(Product p)
+        {
+            ProductDetailsVM pd = new ProductDetailsVM
+            {
+                ProductID = p.Product_Id,
+                Series = p.Series,
+                Model = p.Model,
+                Product_image = p.Product_Image,
+                Model_Year = p.Model_Year,
+            };
+            return pd;
+
+        }
+    }
+}
---
+++ Joole_Project/Scripts/Product.js
@@ -0,0 +1,9 @@
+﻿
+$(document).ready(function () {
+    $("#product-type-button").click(function () {
+        $('#product-types').slideToggle('slow');
+    });
+    $("#tech-specs-label").click(function () {
+        $('#tech-specs-dropdown').slideToggle('slow');
+    });
+});
